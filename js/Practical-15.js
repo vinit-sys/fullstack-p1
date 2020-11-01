cde63e33795a2d3548ae7554d62d02def107f171
vinit-sys <vinit.kumar.cs18@gmail.com>
2020-11-01 18:29:55 +0530

    completed

---
+++ js/Practical-15.js
@@ -0,0 +1,7 @@
+var decre = document.getElementById("decre-btn");
+var incre = document.getElementById("incre-btn");
+
+function incre() {
+  decre.disabled = true;
+  decre.style.backgroundColor = "rgb(8, 8, 8,0.5)";
+}
